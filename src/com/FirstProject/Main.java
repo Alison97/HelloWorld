43c5f123f9b8752886dd32907bf27f5f7359cc3c
Alison97 <alison16.d@gmail.com>
2023-07-13 23:35:26 -0500

    Mortgage calculator

--- src/com/FirstProject/Main.java
+++ src/com/FirstProject/Main.java
@@ -1,9 +1,34 @@
 package com.FirstProject;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi Alison from dev!!! you did it");
+        //Mortgage calculator
+
+        //get the amount
+        System.out.print("Enter the required amount: ");
+        Scanner amountScanner = new Scanner (System.in);
+        Integer amount = amountScanner.nextInt();
+
+        //get the anual interest rate
+        System.out.print("Annual interest rate: ");
+        Scanner interestScanner = new Scanner(System.in);
+        Float monthlyInterestRate = ((interestScanner.nextFloat()/100)/12);
+
+        //get the time (Years)
+        System.out.print("Enter Periods (Years): ");
+        Scanner periodScanner = new Scanner(System.in);
+        Integer periodInMonths = (periodScanner.nextInt()*12);
+
+        //get Mortgage
+        float power  = (float) Math.pow((1 +monthlyInterestRate),periodInMonths);
+        Float mortgageResult = amount * (monthlyInterestRate * power) / ( power - 1);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String currencyResult = currency.format(mortgageResult);
+        System.out.println("Your calculated mortgage is: " + currencyResult);
+
+
     }
 }
