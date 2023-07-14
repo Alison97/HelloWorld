f03b3825206656fd97d0eb015c8ebb047e8edba7
Alison97 <alison16.d@gmail.com>
2023-07-13 20:35:55 -0500

    Main.java added

--- src/com/FirstProject/Main.java
+++ src/com/FirstProject/Main.java
@@ -1,4 +1,4 @@
-package com.FirstProyect;
+package com.FirstProject;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
